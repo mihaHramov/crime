a26e28c223bec429721a8598ccfdc1814f8e8201
miha <vadim.hramov.69@mail.ru>
2019-01-22 22:20:38 +0200

    save url of photo

--- app/src/main/java/com/example/miha/criminalintent/data/repositories/repositoryOfUser/RepositoryOfUser.java
+++ app/src/main/java/com/example/miha/criminalintent/data/repositories/repositoryOfUser/RepositoryOfUser.java
@@ -10,7 +10,9 @@ import rx.Observable;
 public class RepositoryOfUser implements IRepositoryOfUser {
     private static final String MY_SETTINGS = "my_settings";
     private static final String USER_NAME = "userName";
+    private static final String USER_PHOTO = "userPhoto";
     private static final String USER_ID = "id";
+    private static final String USER_SERVER_ID = "id";
     private SharedPreferences sp;
 
     public RepositoryOfUser(Context context) {
@@ -19,17 +21,23 @@ public class RepositoryOfUser implements IRepositoryOfUser {
 
     @Override
     public Observable<User> getCurrentUser() {
-        User user = new User();
-        user.setName(sp.getString(USER_NAME,""));
-        user.setId(sp.getInt(USER_ID,0));
-        return Observable.just(user);
+        return Observable.fromCallable(() -> {
+            User user = new User();
+            user.setName(sp.getString(USER_NAME, ""));
+            user.setId(sp.getInt(USER_ID, 0));
+            user.setServerId(sp.getString(USER_SERVER_ID, ""));
+            user.setPhoto(sp.getString(USER_PHOTO, ""));
+            return user;
+        });
     }
 
     @Override
     public void setCurrentUser(User user) {
-        SharedPreferences.Editor ed =   sp.edit();
-        ed.putString(USER_NAME,user.getName());
-        ed.putInt(USER_ID,user.getId());
+        SharedPreferences.Editor ed = sp.edit();
+        ed.putString(USER_NAME, user.getName());
+        ed.putInt(USER_ID, user.getId());
+        ed.putString(USER_SERVER_ID, user.getServerId());
+        ed.putString(USER_PHOTO, user.getPhoto());
         ed.apply();
     }
 }
