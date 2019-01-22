f196c6457c1022e41e1e3effb368ee64292e36d7
miha <vadim.hramov.69@mail.ru>
2019-01-22 10:50:50 +0200

    get subComponent

--- app/src/main/java/com/example/miha/criminalintent/presentation/ui/ApplicationCrime.java
+++ app/src/main/java/com/example/miha/criminalintent/presentation/ui/ApplicationCrime.java
@@ -4,15 +4,19 @@ import com.example.miha.criminalintent.di.application.ApplicationComponent;
 import com.example.miha.criminalintent.di.application.ContextModule;
 import com.example.miha.criminalintent.di.application.DaggerApplicationComponent;
 import com.example.miha.criminalintent.di.auth.AuthComponent;
+import com.example.miha.criminalintent.di.crimeFragment.CrimeFragmentComponent;
+import com.example.miha.criminalintent.di.crimeFragment.CrimeFragmentModule;
 import com.example.miha.criminalintent.di.crimeListActivity.CrimeListActivityComponent;
 import com.example.miha.criminalintent.di.crimeListFragment.CrimeListFragmentComponnent;
-import com.example.miha.criminalintent.presentation.ui.activity.CrimeListActivity;
+import com.example.miha.criminalintent.di.crimePagerActivity.CrimePagerActivityComponent;
+import com.example.miha.criminalintent.di.crimePagerActivity.CrimePagerActivityModule;
+import com.example.miha.criminalintent.domain.model.Crime;
 
 public class ApplicationCrime extends android.app.Application {
     static ApplicationComponent component;
 
     public static AuthComponent getAuthComponent() {
-      return  component.getAuthComponent();
+        return component.getAuthComponent();
     }
 
     @Override
@@ -24,13 +28,22 @@ public class ApplicationCrime extends android.app.Application {
     }
 
     public static ApplicationComponent getApplicationComponent() {
-        return component;//ApplicationComponent
+        return component;
     }
 
     public static CrimeListFragmentComponnent getCrimeListFragmentComponent() {
         return component.getCrimeListFragmentComponnent();
     }
-    public static CrimeListActivityComponent getCrimeListActivityComponent(){
-      return   component.getCrimeListActivityComponent();
+
+    public static CrimeListActivityComponent getCrimeListActivityComponent() {
+        return component.getCrimeListActivityComponent();
+    }
+
+    public static CrimeFragmentComponent getCrimeFragmentComponent(Crime crime) {
+        return component.getCrimeFragmentComponent(new CrimeFragmentModule(crime));
+    }
+
+    public static CrimePagerActivityComponent getCrimePagerActivityComponent(Crime crime) {
+        return component.getPagerActivityComponent(new CrimePagerActivityModule(crime));
     }
 }
