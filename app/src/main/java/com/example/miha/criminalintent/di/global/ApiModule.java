789b0b4dd113c26c47112bfaf35081495da0a6bf
miha <vadim.hramov.69@mail.ru>
2019-01-22 09:48:11 +0200

    change api mock

--- app/src/main/java/com/example/miha/criminalintent/di/global/ApiModule.java
+++ app/src/main/java/com/example/miha/criminalintent/di/global/ApiModule.java
@@ -3,11 +3,7 @@ package com.example.miha.criminalintent.di.global;
 import com.example.miha.criminalintent.data.network.CrimeApi;
 import com.example.miha.criminalintent.data.network.user.FirebaseAuthEmail;
 import com.example.miha.criminalintent.data.network.user.UserApi;
-import com.example.miha.criminalintent.domain.model.Crime;
 import com.google.firebase.auth.FirebaseAuth;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import dagger.Module;
 import dagger.Provides;
@@ -17,27 +13,17 @@ import rx.Observable;
 public class ApiModule {
 
     @Provides
-    public CrimeApi provideCrimeApi() {
-        return () -> {
-            Crime crime = new Crime();
-            crime.setSolved(false);
-            crime.setTitle("yes");
-            //crime.setSuspect("suspect");
-            List<Crime> crimeList = new ArrayList<>();
-            for (int i = 0;i<20;i++){
-                crimeList.add(crime);
-            }
-            return Observable.just(crimeList);
-        };
+    CrimeApi provideCrimeApi() {
+        return () -> Observable.error(new Throwable());
     }
 
     @Provides
-    public FirebaseAuth provideFirebaseAuth(){
-      return   FirebaseAuth.getInstance();
+    FirebaseAuth provideFirebaseAuth() {
+        return FirebaseAuth.getInstance();
     }
 
     @Provides
-    public UserApi provideUserApi(FirebaseAuth auth){
+    UserApi provideUserApi(FirebaseAuth auth) {
         return new FirebaseAuthEmail(auth);
     }
 }
