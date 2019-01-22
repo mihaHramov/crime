7cf4482ca78b1365d6721dd5cc9fa229c77d108b
miha <vadim.hramov.69@mail.ru>
2019-01-22 10:49:35 +0200

    change application component

--- app/src/main/java/com/example/miha/criminalintent/di/application/ApplicationComponent.java
+++ app/src/main/java/com/example/miha/criminalintent/di/application/ApplicationComponent.java
@@ -1,8 +1,12 @@
 package com.example.miha.criminalintent.di.application;
 
 import com.example.miha.criminalintent.di.auth.AuthComponent;
+import com.example.miha.criminalintent.di.crimeFragment.CrimeFragmentComponent;
+import com.example.miha.criminalintent.di.crimeFragment.CrimeFragmentModule;
 import com.example.miha.criminalintent.di.crimeListActivity.CrimeListActivityComponent;
 import com.example.miha.criminalintent.di.crimeListFragment.CrimeListFragmentComponnent;
+import com.example.miha.criminalintent.di.crimePagerActivity.CrimePagerActivityComponent;
+import com.example.miha.criminalintent.di.crimePagerActivity.CrimePagerActivityModule;
 import com.example.miha.criminalintent.di.global.ApiModule;
 import com.example.miha.criminalintent.di.global.RepositoryModule;
 import com.example.miha.criminalintent.di.global.SchedulerModule;
@@ -18,4 +22,6 @@ public interface ApplicationComponent {
 
     AuthComponent getAuthComponent();
     CrimeListActivityComponent getCrimeListActivityComponent();
+    CrimeFragmentComponent getCrimeFragmentComponent(CrimeFragmentModule crimeFragmentModule);
+    CrimePagerActivityComponent getPagerActivityComponent(CrimePagerActivityModule module);
 }
