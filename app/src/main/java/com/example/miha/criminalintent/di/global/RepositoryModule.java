ef5e1faecf3451dc4ea8c1cb0fda9a6ee634e4b4
miha <vadim.hramov.69@mail.ru>
2019-01-22 22:24:57 +0200

    provide IRepositoryOfCrime

--- app/src/main/java/com/example/miha/criminalintent/di/global/RepositoryModule.java
+++ app/src/main/java/com/example/miha/criminalintent/di/global/RepositoryModule.java
@@ -2,56 +2,26 @@ package com.example.miha.criminalintent.di.global;
 
 import android.content.Context;
 
+import com.example.miha.criminalintent.data.repositories.repositoryOfCrime.BdRepositoryOfCrime;
 import com.example.miha.criminalintent.data.repositories.repositoryOfCrime.IRepositoryOfCrime;
+import com.example.miha.criminalintent.data.repositories.repositoryOfCrime.RepositoryOfCrime;
 import com.example.miha.criminalintent.data.repositories.repositoryOfUser.IRepositoryOfUser;
 import com.example.miha.criminalintent.data.repositories.repositoryOfUser.RepositoryOfUser;
-import com.example.miha.criminalintent.domain.model.Crime;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import dagger.Module;
 import dagger.Provides;
-import rx.Observable;
 
 @Module
 public class RepositoryModule {
     @Provides
-    IRepositoryOfCrime provideRepositoryOfCrime() {
-        return new IRepositoryOfCrime() {
-            @Override
-            public Observable<List<Crime>> getCrimes() {
-                List<Crime> crimeList = new ArrayList<>();
-                for (int i = 0; i < 30; i++) {
-                    Crime crime = new Crime();
-                    crime.setTitle("temp" + i);
-                    crime.setId(i);
-                    crime.setSolved(false);
-                    crime.setPhoto("photo");
-                    crimeList.add(crime);
-                }
-                return Observable.just(crimeList);
-            }
-
-            @Override
-            public Boolean update(Crime crime) {
-                return null;
-            }
-
-            @Override
-            public Boolean delete(Crime crime) {
-                return null;
-            }
-
-            @Override
-            public Observable<Crime> create() {
-                Crime crime = new Crime();
-                crime.setTitle("temp");
-                return Observable.just(crime);
-            }
-        };
+    IRepositoryOfCrime provideRepositoryOfCrime(BdRepositoryOfCrime bd) {
+        return new RepositoryOfCrime(bd);
     }
 
+    @Provides
+    BdRepositoryOfCrime provide(Context context){
+        return new BdRepositoryOfCrime(context);
+    }
     @Provides
     IRepositoryOfUser provideRepositoryOfUser(Context context) {
         return new RepositoryOfUser(context);
