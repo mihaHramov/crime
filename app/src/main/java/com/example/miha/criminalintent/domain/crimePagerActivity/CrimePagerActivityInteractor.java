6f73de2b321a9becd34cc3b7efa06d9c3ee0af9e
miha <vadim.hramov.69@mail.ru>
2019-01-23 20:36:15 +0200

    clear code

--- app/src/main/java/com/example/miha/criminalintent/domain/crimePagerActivity/CrimePagerActivityInteractor.java
+++ app/src/main/java/com/example/miha/criminalintent/domain/crimePagerActivity/CrimePagerActivityInteractor.java
@@ -1,7 +1,5 @@
 package com.example.miha.criminalintent.domain.crimePagerActivity;
 
-import android.util.Log;
-
 import com.example.miha.criminalintent.data.repositories.repositoryOfCrime.IRepositoryOfCrime;
 import com.example.miha.criminalintent.domain.model.Crime;
 import com.example.miha.criminalintent.presentation.ui.utils.ISchedulersProvider;
@@ -23,16 +21,15 @@ public class CrimePagerActivityInteractor implements ICrimePagerActivityInteract
     @Override
     public void getCrimes(Callback callback) {
         repositoryOfCrime.getCrimes()
-                .doOnNext(crimes -> Log.d("mihaHramov pager",crimes.size()+""))
-                    .subscribeOn(schedulers.newThread())
-                    .observeOn(schedulers.ui())
-                    .subscribe(callback::success);
+                .subscribeOn(schedulers.newThread())
+                .observeOn(schedulers.ui())
+                .subscribe(callback::success);
     }
 
     @Override
     public Integer getCurrentPosition(List<Crime> crimes) {
-        for(int i=0;i<crimes.size();i++){
-            if(crimes.get(i).getId().equals(crime.getId())){
+        for (int i = 0; i < crimes.size(); i++) {
+            if (crimes.get(i).getId().equals(crime.getId())) {
                 return i;
             }
         }
