7ef33d696569d18756f3efd0af060a523add1d5b
miha <vadim.hramov.69@mail.ru>
2019-01-23 19:10:32 +0200

    change method "getCrimes"

--- app/src/main/java/com/example/miha/criminalintent/data/repositories/repositoryOfCrime/BdRepositoryOfCrime.java
+++ app/src/main/java/com/example/miha/criminalintent/data/repositories/repositoryOfCrime/BdRepositoryOfCrime.java
@@ -5,10 +5,15 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.miha.criminalintent.domain.model.Comment;
 import com.example.miha.criminalintent.domain.model.Crime;
+import com.example.miha.criminalintent.domain.model.User;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 
 
 public class BdRepositoryOfCrime extends SQLiteOpenHelper {
@@ -46,42 +51,172 @@ public class BdRepositoryOfCrime extends SQLiteOpenHelper {
     public List<Crime> getCrimes() {
         SQLiteDatabase db = this.getWritableDatabase();
         List<Crime> crimeList = new ArrayList<>();
-        String authorName = "author_name";
-        String crimeTitle = "crime_name";
-        String suspectName = "suspect_name";
+
+        String crimeAs = "crime";
+        String authorAs = "author";
+        String userSuspect = "suspect";
 
 
-        String crimeAs = "crimeAs";
-        String userAs = "userAs";
-        String userSuspect = "suspectAs";
+        String authorName = authorAs + "_" + USER_NAME;
+        String authorid = authorAs + "_" + USER_ID;
+        String authorPhoto = authorAs + "_" + USER_PHOTO;
+        String authorUrlId = authorAs + "_" + USER_URL_ID;
+
+        String suspectName = userSuspect + "_" + USER_NAME;
+        String suspectid = userSuspect + "_" + USER_ID;
+        String suspectPhoto = userSuspect + "_" + USER_PHOTO;
+        String suspectUrlId = userSuspect + "_" + USER_URL_ID;
+
+        String crimeName = crimeAs + "_" + CRIME_TITLE;
+        String crimeDate = crimeAs + "_" + CRIME_DATE;
+        String crimePhoto = crimeAs + "_" + CRIME_PHOTO;
+        String crimeSolved = crimeAs + "_" + CRIME_SOLVED;
+        String crimeId = crimeAs + "_" + CRIME_ID;
 
         String select = "SELECT " +
-                userAs + "." + USER_NAME + " AS " + authorName + " , " +
-                crimeAs + "." + CRIME_TITLE + " AS " + crimeTitle + " , " +
-                userSuspect + "." + USER_NAME + " AS " + suspectName;
+                authorAs + "." + USER_NAME + " AS " + authorName + " , " +
+                authorAs + "." + USER_ID + " AS " + authorid + " , " +
+                authorAs + "." + USER_PHOTO + " AS " + authorPhoto + " , " +
+                authorAs + "." + USER_URL_ID + " AS " + authorUrlId + " , " +
+
+                userSuspect + "." + USER_NAME + " AS " + suspectName + " , " +
+                userSuspect + "." + USER_ID + " AS " + suspectid + " , " +
+                userSuspect + "." + USER_PHOTO + " AS " + suspectPhoto + " , " +
+                userSuspect + "." + USER_URL_ID + " AS " + suspectUrlId + " , " +
+
+                crimeAs + "." + CRIME_TITLE + " AS " + crimeName + " , " +
+                crimeAs + "." + CRIME_DATE + " AS " + crimeDate + " , " +
+                crimeAs + "." + CRIME_PHOTO + " AS " + crimePhoto + " , " +
+                crimeAs + "." + CRIME_SOLVED + " AS " + crimeSolved + " , " +
+                crimeAs + "." + CRIME_ID + " AS " + crimeId;
+
         String from = " FROM " + CRIME_TABLE + " AS " + crimeAs +
-                " INNER JOIN " + USER_TABLE + " AS " + userAs + " ON " + crimeAs + "." + CRIME_AUTHOR + "=" + userAs + "." + USER_ID +
+                " INNER JOIN " + USER_TABLE + " AS " + authorAs + " ON " + crimeAs + "." + CRIME_AUTHOR + "=" + authorAs + "." + USER_ID +
                 " LEFT JOIN " + USER_TABLE + " AS " + userSuspect + " ON " + crimeAs + "." + CRIME_SUSPECT + "=" + userSuspect + "." + USER_ID;
 
 
         Cursor cursor = db.rawQuery(select + from, null);
         if (cursor.moveToFirst()) {
+            Integer authoeNameidCol = cursor.getColumnIndex(authorName);
+            Integer authorIdCol = cursor.getColumnIndex(authorid);
+            Integer authorPhotoCol = cursor.getColumnIndex(authorPhoto);
+            Integer authorServerIdCol = cursor.getColumnIndex(authorUrlId);
 
+            Integer suspectNameidCol = cursor.getColumnIndex(suspectName);
+            Integer suspectIdCol = cursor.getColumnIndex(suspectid);
+            Integer suspectPhotoCol = cursor.getColumnIndex(suspectPhoto);
+            Integer suspectServerIdCol = cursor.getColumnIndex(suspectUrlId);
+
+
+            Integer titleIdCol = cursor.getColumnIndex(crimeName);
+            Integer crimeDateCol = cursor.getColumnIndex(crimeDate);
+            Integer crimePhotoColl = cursor.getColumnIndex(crimePhoto);
+            Integer crimeisSolvedColl = cursor.getColumnIndex(crimeSolved);
+            Integer crimeIdColl = cursor.getColumnIndex(crimeId);
+
+            String temp = "";
+            do {
+                User author = getUserFromCrime(cursor, authoeNameidCol, authorIdCol, authorPhotoCol, authorServerIdCol);
+                User suspect = getUserFromCrime(cursor, suspectNameidCol, suspectIdCol, suspectPhotoCol, suspectServerIdCol);
+                Crime crime = getCrime(cursor, titleIdCol, crimeDateCol, crimePhotoColl, crimeisSolvedColl, crimeIdColl);
+                crime.setAuthor(author);
+                crime.setSuspect(suspect);
+                crime.setComments(new ArrayList<>());
+                crimeList.add(crime);
+                temp = temp.concat(crime.getId() + ",");
+            } while (cursor.moveToNext());
+            temp = temp.substring(0, temp.length() - 1);
+
+
+            String message = "message";
+            String commentId = "comment_id";
+            String commentData = "comment_data";
+            String commentIdCrime = "crime_id";
+
+            //getComments
+            String selectComments = " SELECT  " +
+                    COMMENT_TABLE + "." + COMMENT_MESSAGE + " AS " + message + " , " +
+                    COMMENT_TABLE + "." + COMMENT_ID + " AS " + commentId + " , " +
+                    COMMENT_TABLE + "." + COMMENT_DATA + " AS " + commentData + " , " +
+                    COMMENT_TABLE + "." + COMMENT_CRIME + " AS " + commentIdCrime + " , " +
+                    authorAs + "." + USER_NAME + " AS " + authorName + " , " +
+                    authorAs + "." + USER_ID + " AS " + authorid + " , " +
+                    authorAs + "." + USER_PHOTO + " AS " + authorPhoto + " , " +
+                    authorAs + "." + USER_URL_ID + " AS " + authorUrlId;
+
+
+            String columnComments = " FROM " + COMMENT_TABLE + " INNER JOIN " + USER_TABLE + " AS " + authorAs +
+                    " ON " + COMMENT_TABLE + "." + COMMENT_USER + "=" + authorid;
+
+            String whereComments = " WHERE " + commentIdCrime + " IN (" + temp + ")";
+            String orderBy = " ORDER BY " + commentIdCrime;
+
+
+            Cursor cursorComment = db.rawQuery(selectComments + columnComments + whereComments + orderBy, null);
+            if (cursorComment.moveToFirst()) {
+                Integer messageCol = cursorComment.getColumnIndex(COMMENT_TABLE + "." + COMMENT_MESSAGE);
+                Integer dataCol = cursorComment.getColumnIndex(COMMENT_TABLE + "." + COMMENT_DATA);
+                Integer crimeIdCol = cursorComment.getColumnIndex(COMMENT_TABLE + "." + COMMENT_CRIME);
+                Integer idCommentCol = cursorComment.getColumnIndex(commentId);
+
+                Integer authorCol = cursorComment.getColumnIndex(authorName);
+                Integer authoridCol = cursorComment.getColumnIndex(authorid);
+                Integer photoCol = cursorComment.getColumnIndex(authorPhoto);
+                Integer urlId = cursorComment.getColumnIndex(authorUrlId);
+
+                Map<Integer, List<Comment>> map = new Hashtable<>();
+                do {
+                    Comment comment = new Comment();
+                    comment.setDate(cursorComment.getString(dataCol));
+                    comment.setMessage(cursorComment.getString(messageCol));
+                    comment.setId(cursorComment.getInt(idCommentCol));
+                    Integer idCrime = cursorComment.getInt(crimeIdCol);
+                    User user = getUserFromCrime(cursorComment, authorCol, authoridCol, photoCol, urlId);
+                    comment.setAuthor(user);
+                    if (!map.containsKey(idCrime)) {
+                        List<Comment> comments = new ArrayList<>();
+                        comments.add(comment);
+                        map.put(idCrime, comments);
+                    } else {
+                        map.get(idCrime).add(comment);
+                    }
+
+                } while (cursorComment.moveToNext());
+
+                for (Crime cr : crimeList) {
+                    if (map.containsKey(cr.getId())) {
+                        cr.setComments(map.get(cr.getId()));
+                    }
+                }
+            }
+            cursorComment.close();
         }
         cursor.close();
-
-
-        for (int i = 0; i < 30; i++) {
-            Crime crime = new Crime();
-            crime.setTitle("temp" + i);
-            crime.setId(i);
-            crime.setSolved(false);
-            crime.setPhoto("photo");
-            crimeList.add(crime);
-        }
         return crimeList;
     }
 
+    private Crime getCrime(Cursor cursor, Integer titleIdCol, Integer crimeDateCol, Integer crimePhotoColl, Integer crimeisSolvedColl, Integer crimeIdColl) {
+        Crime crime = new Crime();
+        crime.setTitle(cursor.getString(titleIdCol));
+        crime.setDate(cursor.getString(crimeDateCol));
+        crime.setPhoto(cursor.getString(crimePhotoColl));
+        crime.setSolved(cursor.getString(crimeisSolvedColl).equals("true"));
+        crime.setId(cursor.getInt(crimeIdColl));
+        return crime;
+    }
+
+    private User getUserFromCrime(Cursor cursor, Integer authoeNameidCol, Integer authorIdCol, Integer authorPhotoCol, Integer authorServerIdCol) {
+        User author = new User();
+        author.setName(getSting(authoeNameidCol, cursor));
+        author.setId(cursor.getInt(authorIdCol));
+        author.setPhoto(getSting(authorPhotoCol, cursor));
+        author.setServerId(getSting(authorServerIdCol, cursor));
+        return author;
+    }
+
+    private String getSting(Integer id, Cursor cursor) {
+        return cursor.getString(id);
+    }
 
     public Boolean update(Crime crime) {
         return null;
@@ -95,8 +230,16 @@ public class BdRepositoryOfCrime extends SQLiteOpenHelper {
         SQLiteDatabase db = getWritableDatabase();
         Crime crime = new Crime();
         crime.setTitle(CRIME_TITLE);
+        crime.setPhoto("");
+        crime.setDate("");
+        crime.setSolved(false);
         ContentValues cv = new ContentValues();
+
         cv.put(CRIME_TITLE, crime.getTitle());
+        cv.put(CRIME_SOLVED, crime.getSolved());
+        cv.put(CRIME_DATE, crime.getDate());
+        cv.put(CRIME_PHOTO, crime.getPhoto());
+        cv.put(CRIME_AUTHOR, 0);
         crime.setId((int) db.insert(CRIME_TABLE, null, cv));
         db.close();
         return crime;
