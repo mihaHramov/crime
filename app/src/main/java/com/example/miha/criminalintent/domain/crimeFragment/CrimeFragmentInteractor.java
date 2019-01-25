61e0b3c6ab4b57c1b37da05e4053cc4492fec8ea
miha <vadim.hramov.69@mail.ru>
2019-01-25 15:23:23 +0200

    create method "isChangeableCrime"

--- app/src/main/java/com/example/miha/criminalintent/domain/crimeFragment/CrimeFragmentInteractor.java
+++ app/src/main/java/com/example/miha/criminalintent/domain/crimeFragment/CrimeFragmentInteractor.java
@@ -1,16 +1,20 @@
 package com.example.miha.criminalintent.domain.crimeFragment;
 
 import com.example.miha.criminalintent.data.repositories.repositoryOfCrime.IRepositoryOfCrime;
+import com.example.miha.criminalintent.data.repositories.repositoryOfUser.IRepositoryOfUser;
 import com.example.miha.criminalintent.domain.model.Crime;
+import com.example.miha.criminalintent.domain.model.User;
 import com.example.miha.criminalintent.presentation.ui.utils.ISchedulersProvider;
 
 public class CrimeFragmentInteractor implements ICrimeFragmentInteractor {
     private Crime crime;
+    private IRepositoryOfUser repositoryOfUser;
     private IRepositoryOfCrime repositoryOfCrime;
     private ISchedulersProvider schedulersProvider;
 
-    public CrimeFragmentInteractor(Crime crime, IRepositoryOfCrime repositoryOfCrime, ISchedulersProvider schedulersProvider) {
+    public CrimeFragmentInteractor(Crime crime, IRepositoryOfCrime repositoryOfCrime, ISchedulersProvider schedulersProvider, IRepositoryOfUser repositoryOfUser) {
         this.crime = crime;
+        this.repositoryOfUser = repositoryOfUser;
         this.repositoryOfCrime = repositoryOfCrime;
         this.schedulersProvider = schedulersProvider;
     }
@@ -26,5 +30,17 @@ public class CrimeFragmentInteractor implements ICrimeFragmentInteractor {
                 .subscribeOn(schedulersProvider.newThread())
                 .observeOn(schedulersProvider.ui())
                 .subscribe();
+    }
+
+    private Boolean isEnabledCrime(User user) {
+        return crime.getAuthor().getId().equals(user.getId()) || crime.getAuthor().getId() == 0;
+    }
+
+    @Override
+    public void isChangeableCrime(BoolCallback callback) {
+        repositoryOfUser.getCurrentUser()
+                .subscribeOn(schedulersProvider.newThread())
+                .observeOn(schedulersProvider.ui())
+                .subscribe(user -> callback.answer(this.isEnabledCrime(user)));
     }
 }
--- app/src/main/java/com/example/miha/criminalintent/domain/crimeFragment/ICrimeFragmentInteractor.java
+++ app/src/main/java/com/example/miha/criminalintent/domain/crimeFragment/ICrimeFragmentInteractor.java
@@ -3,6 +3,13 @@ package com.example.miha.criminalintent.domain.crimeFragment;
 import com.example.miha.criminalintent.domain.model.Crime;
 
 public interface ICrimeFragmentInteractor {
+
+    void isChangeableCrime(BoolCallback callback);
+
+    interface BoolCallback{
+        void answer(Boolean flag);
+    }
+
     void saveCrime();
 
     interface LoadCrimeListener {
