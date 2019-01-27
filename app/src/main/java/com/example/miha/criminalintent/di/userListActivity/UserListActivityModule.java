b94f9174a90c2c4d1b40ed5f0647adef2b710ed0
miha <vadim.hramov.69@mail.ru>
2019-01-27 21:01:55 +0200

    add UserListActivityModule

---
+++ app/src/main/java/com/example/miha/criminalintent/di/userListActivity/UserListActivityModule.java
@@ -0,0 +1,17 @@
+package com.example.miha.criminalintent.di.userListActivity;
+
+import com.example.miha.criminalintent.data.repositories.repositoryOfUser.IRepositoryOfUser;
+import com.example.miha.criminalintent.domain.userListActivity.IUserListActivityInteractor;
+import com.example.miha.criminalintent.domain.userListActivity.UserListActivityInteractor;
+import com.example.miha.criminalintent.presentation.ui.utils.ISchedulersProvider;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class UserListActivityModule {
+    @Provides
+    IUserListActivityInteractor providUserListActivityInteractor(IRepositoryOfUser repositoryOfUser, ISchedulersProvider provider) {
+        return new UserListActivityInteractor(repositoryOfUser, provider);
+    }
+}
