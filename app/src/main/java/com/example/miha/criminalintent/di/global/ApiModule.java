e8faf6686783b510895503ddd91741735b58c0b4
miha <vadim.hramov.69@mail.ru>
2019-01-27 16:34:50 +0200

    provide new api

--- app/src/main/java/com/example/miha/criminalintent/di/global/ApiModule.java
+++ app/src/main/java/com/example/miha/criminalintent/di/global/ApiModule.java
@@ -1,9 +1,14 @@
 package com.example.miha.criminalintent.di.global;
 
 import com.example.miha.criminalintent.data.network.CrimeApi;
+import com.example.miha.criminalintent.data.network.crime.ICrimeApi;
 import com.example.miha.criminalintent.data.network.user.FirebaseAuthEmail;
-import com.example.miha.criminalintent.data.network.user.UserApi;
+import com.example.miha.criminalintent.data.network.user.AuthUserApi;
+import com.example.miha.criminalintent.data.network.user.UserStorage;
+import com.example.miha.criminalintent.data.network.user.UserStorageApi;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -20,17 +25,37 @@ public class ApiModule {
     }
 
     @Provides
+    ICrimeApi povideICrimeApi() {
+        return new ICrimeApi() {
+            @Override
+            public Observable<String> shareCrime() {
+                return null;
+            }
+        };
+    }
+
+    @Provides
+    DatabaseReference provideDatabeseReference() {
+        return FirebaseDatabase.getInstance().getReference();
+    }
+
+    @Provides
     FirebaseAuth provideFirebaseAuth() {
         return FirebaseAuth.getInstance();
     }
 
     @Provides
-    UserApi provideUserApi(FirebaseAuth auth) {
+    AuthUserApi provideUserApi(FirebaseAuth auth) {
         return new FirebaseAuthEmail(auth);
     }
 
     @Provides
     StorageReference provideStorageReference() {
         return FirebaseStorage.getInstance().getReference();
+    }
+
+    @Provides
+    UserStorageApi provideUserStorageApi(DatabaseReference dbRef){
+        return new UserStorage(dbRef);
     }
 }
