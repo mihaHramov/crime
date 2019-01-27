241e4f372a302d2f1126826b11e2435b68f79672
miha <vadim.hramov.69@mail.ru>
2019-01-27 16:37:41 +0200

    add methods to show when crime is send

--- app/src/main/java/com/example/miha/criminalintent/domain/model/ItemCrime.java
+++ app/src/main/java/com/example/miha/criminalintent/domain/model/ItemCrime.java
@@ -3,6 +3,15 @@ package com.example.miha.criminalintent.domain.model;
 public class ItemCrime {
     private Crime crime;
     private Integer position;
+    private Boolean inProgress;
+
+    public Boolean getInProgress() {
+        return inProgress;
+    }
+
+    public void setInProgress(Boolean inProgress) {
+        this.inProgress = inProgress;
+    }
 
     public Crime getCrime() {
         return crime;
