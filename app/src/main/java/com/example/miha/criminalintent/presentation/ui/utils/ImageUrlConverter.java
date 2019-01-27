c1587b34824dd04333d805ab17614ac724b3bf84
miha <vadim.hramov.69@mail.ru>
2019-01-27 16:45:53 +0200

    add method "isUrl"

--- app/src/main/java/com/example/miha/criminalintent/presentation/ui/utils/ImageUrlConverter.java
+++ app/src/main/java/com/example/miha/criminalintent/presentation/ui/utils/ImageUrlConverter.java
@@ -4,8 +4,13 @@ public class ImageUrlConverter {
     private static final String url = "http://";
     private static final String url_secret = "https://";
 
+
+    public static Boolean isUrl(String filename) {
+        return filename.startsWith(url) || filename.startsWith(url_secret);
+    }
+
     public static String getRealAddressFile(String filename) {
-        if (filename.startsWith(url) || filename.startsWith(url_secret)) {
+        if (isUrl(filename)) {
             return filename;
         } else {
             return "file://" + filename;
