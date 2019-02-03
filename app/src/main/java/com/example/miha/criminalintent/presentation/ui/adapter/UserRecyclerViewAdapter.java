addc64d5aa6db8588cb657ab08a3937b49b5d1bc
miha <vadim.hramov.69@mail.ru>
2019-02-03 18:46:35 +0200

    create new adapter

---
+++ app/src/main/java/com/example/miha/criminalintent/presentation/ui/adapter/UserRecyclerViewAdapter.java
@@ -0,0 +1,61 @@
+package com.example.miha.criminalintent.presentation.ui.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.miha.criminalintent.R;
+import com.example.miha.criminalintent.domain.model.User;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class UserRecyclerViewAdapter extends RecyclerView.Adapter<UserRecyclerViewAdapter.ViewHolder> {
+    private List<User> users = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.user_item, viewGroup, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        User user = users.get(i);
+        if (!user.getPhoto().isEmpty()) {
+            Picasso.get().load(user.getPhoto()).into(viewHolder.imageView);
+        }
+        viewHolder.name.setText(user.getName());
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.name)
+        TextView name;
+        @BindView(R.id.image)
+        ImageView imageView;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
