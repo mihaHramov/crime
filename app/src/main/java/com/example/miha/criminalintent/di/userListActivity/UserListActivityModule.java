dbf4a641c4f7baad393559021c655fa21c794942
miha <vadim.hramov.69@mail.ru>
2019-02-03 22:44:29 +0200

    provide "UserRecyclerViewAdapter"

--- app/src/main/java/com/example/miha/criminalintent/di/userListActivity/UserListActivityModule.java
+++ app/src/main/java/com/example/miha/criminalintent/di/userListActivity/UserListActivityModule.java
@@ -4,6 +4,7 @@ import com.example.miha.criminalintent.data.repositories.repositoryOfUser.IRepos
 import com.example.miha.criminalintent.domain.userListActivity.IUserListActivityInteractor;
 import com.example.miha.criminalintent.domain.userListActivity.UserListActivityInteractor;
 import com.example.miha.criminalintent.presentation.mvp.userListActivity.UserListActivityPresenter;
+import com.example.miha.criminalintent.presentation.ui.adapter.UserRecyclerViewAdapter;
 import com.example.miha.criminalintent.presentation.ui.utils.ISchedulersProvider;
 
 import dagger.Module;
@@ -15,8 +16,14 @@ public class UserListActivityModule {
     IUserListActivityInteractor providUserListActivityInteractor(IRepositoryOfUser repositoryOfUser, ISchedulersProvider provider) {
         return new UserListActivityInteractor(repositoryOfUser, provider);
     }
+
     @Provides
-    UserListActivityPresenter providePresenter(IUserListActivityInteractor interactor){
+    UserListActivityPresenter providePresenter(IUserListActivityInteractor interactor) {
         return new UserListActivityPresenter(interactor);
+    }
+
+    @Provides
+    UserRecyclerViewAdapter adapter() {
+        return new UserRecyclerViewAdapter();
     }
 }
