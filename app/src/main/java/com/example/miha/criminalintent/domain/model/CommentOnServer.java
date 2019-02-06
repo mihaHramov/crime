ef46a9280bf52c6e4ff3196eb3bf0c95c2804f80
miha <vadim.hramov.69@mail.ru>
2019-02-06 19:23:16 +0200

    add new model to put/get  comments on server

---
+++ app/src/main/java/com/example/miha/criminalintent/domain/model/CommentOnServer.java
@@ -0,0 +1,30 @@
+package com.example.miha.criminalintent.domain.model;
+
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@IgnoreExtraProperties
+public class CommentOnServer {
+    public String authorId;
+    public String authorPhoto;
+    public String authorName;
+    public String date;
+    public String message;
+
+    public CommentOnServer() {
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("authorPhoto", authorPhoto);
+        result.put("authorId", authorId);
+        result.put("authorName", authorName);
+        result.put("date", date);
+        result.put("message", message);
+        return result;
+    }
+}
