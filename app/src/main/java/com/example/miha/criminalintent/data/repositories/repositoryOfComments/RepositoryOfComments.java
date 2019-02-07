c67337d3fbc179b14ca687a75eb6323a73e66ae3
miha <vadim.hramov.69@mail.ru>
2019-02-07 21:15:08 +0200

    use ICommentsApi

--- app/src/main/java/com/example/miha/criminalintent/data/repositories/repositoryOfComments/RepositoryOfComments.java
+++ app/src/main/java/com/example/miha/criminalintent/data/repositories/repositoryOfComments/RepositoryOfComments.java
@@ -1,13 +1,15 @@
 package com.example.miha.criminalintent.data.repositories.repositoryOfComments;
 
 import com.example.miha.criminalintent.data.network.comments.ICommentsApi;
+import com.example.miha.criminalintent.domain.mapper.CommentsMapper;
 import com.example.miha.criminalintent.domain.model.Comment;
+import com.example.miha.criminalintent.domain.model.CommentOnServer;
 import com.example.miha.criminalintent.domain.model.Crime;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import rx.Observable;
+import rx.functions.Func1;
 
 public class RepositoryOfComments implements IRepositoryOfComments {
     private ICommentsApi commentsApi;
@@ -18,7 +20,10 @@ public class RepositoryOfComments implements IRepositoryOfComments {
 
     @Override
     public Observable<List<Comment>> getComments(Crime crime) {
-        List<Comment> list = new ArrayList<>();
-        return Observable.just(list);
+        String serverIdComments = crime.getAuthor().getServerId() + crime.getDate();
+        return commentsApi.getComments(serverIdComments)
+                .flatMap((Func1<List<CommentOnServer>, Observable<CommentOnServer>>) Observable::from)
+                .map(new CommentsMapper())
+                .toList();
     }
 }
