aa9516bd7b2af6298c98c790b8080f9804ef2acf
miha <vadim.hramov.69@mail.ru>
2019-02-08 23:51:51 +0200

    create "CommentsCreateFragment"

---
+++ app/src/main/java/com/example/miha/criminalintent/presentation/ui/dialog/CommentsCreateFragment.java
@@ -0,0 +1,97 @@
+package com.example.miha.criminalintent.presentation.ui.dialog;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.arellomobile.mvp.MvpAppCompatDialogFragment;
+import com.arellomobile.mvp.presenter.InjectPresenter;
+import com.arellomobile.mvp.presenter.ProvidePresenter;
+import com.example.miha.criminalintent.R;
+import com.example.miha.criminalintent.domain.model.Crime;
+import com.example.miha.criminalintent.domain.model.User;
+import com.example.miha.criminalintent.presentation.mvp.commentsCreateFragment.CommentsCreateFragmentPresenter;
+import com.example.miha.criminalintent.presentation.mvp.commentsCreateFragment.CommentsCreateFragmentView;
+import com.example.miha.criminalintent.presentation.ui.ApplicationCrime;
+import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CommentsCreateFragment extends MvpAppCompatDialogFragment implements CommentsCreateFragmentView {
+    private static final String KEY = CommentsCreateFragment.class.getName();
+    @BindView(R.id.image)
+    ImageView image;
+    @BindView(R.id.name)
+    TextView name;
+    @BindView(R.id.message)
+    EditText message;
+
+    @BindView(R.id.send_message)
+    Button btn;
+
+
+    @InjectPresenter
+    CommentsCreateFragmentPresenter presenter;
+
+    @ProvidePresenter
+    CommentsCreateFragmentPresenter provideCommentsCreateFragmentPresenter() {
+        presenter = ApplicationCrime.getCommentsCreateFragmentComponent((Crime) getArguments().getSerializable(KEY)).getPresenter();
+        presenter.init();
+        return presenter;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.create_comments, container, false);
+        ButterKnife.bind(this, v);
+        btn.setOnClickListener(view -> {
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            presenter.sendComment(message.getText().toString(), timeStamp);
+        });
+        return v;
+    }
+
+    @Override
+    public void showError(String message) {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showSuccess() {
+        Toast.makeText(getActivity(), "show message success", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void setEnabled(Boolean flag) {
+        message.setEnabled(flag);
+
+    }
+
+    @Override
+    public void showUserInfo(User user) {
+        Picasso.get().load(user.getPhoto()).into(image);
+        name.setText(user.getName());
+    }
+
+    public static CommentsCreateFragment newInstance(Crime crime) {
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(KEY, crime);
+        CommentsCreateFragment fragment = new CommentsCreateFragment();
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+}
